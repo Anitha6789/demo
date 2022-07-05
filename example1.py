7c97e252b5ff57d51d39facc46bdb95bbe5c0b36
Gadi Anitha <Gadi.anitha@perficient.com>
2022-07-05 15:27:50 +0530

    selection sort added

--- example1.py
+++ example1.py
@@ -6,10 +6,8 @@
 # to come up with a "sorted" nums relative to the pivot
 
 
-<<<<<<< HEAD
 print("this is added before anithas commit")
 print("hello merge ex")
-=======
 def partition(l, r, nums):
 	# Last element will be the pivot and the first element the pointer
 	pivot, ptr = nums[r], l
@@ -45,4 +43,28 @@ example = [2, 5, 6, 1, 4, 6, 2, 4, 7, 8]
 result = [1, 2, 2, 4, 4, 5, 6, 6, 7, 8]
 # As you can see, it works for duplicates too
 print(quicksort(0, len(example)-1, example))
->>>>>>> 0e024554007ee92ed4d667ecc02f27ec9943333b
+ 
+
+ # Python program for implementation of Selection
+# Sort
+import sys
+A = [64, 25, 12, 22, 11]
+ 
+# Traverse through all array elements
+for i in range(len(A)):
+  
+ # Find the minimum element in remaining
+ # unsorted array
+ min_idx = i
+ for j in range(i+1, len(A)):
+  if A[min_idx] > A[j]:
+   min_idx = j
+    
+ # Swap the found minimum element with
+ # the first element  
+ A[i], A[min_idx] = A[min_idx], A[i]
+ 
+# Driver code to test above
+print ("Sorted array")
+for i in range(len(A)):
+ print("%d" %A[i]),
